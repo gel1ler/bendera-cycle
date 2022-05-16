2184434b516603b34a322e0117da0915063b5318
gel1ler <57824633+gel1ler@users.noreply.github.com>
2022-05-16 13:50:16 +0300

    first comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './app.css'
+import video from './assets/video.mp4'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>Bendera-1</h1>
+      <video className='video' autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+      </video>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
