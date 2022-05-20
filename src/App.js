f827e228a089fc21b7d81f804287ddb8cc8add49
bitold <manmajor05@mail.ru>
2022-05-21 00:28:33 +0300

    message

--- src/App.js
+++ src/App.js
@@ -8,19 +8,20 @@ const App = () => {
   return (
     <Box>
       <Header
-        title='B E N D E R A - 1'
+        title='B E &nbsp;&nbsp;&nbsp; N D E R A - aea 1 eae'
         video={video}
       />
       <Box sx={{ my: 4, mx: 'auto', width: '62.5%' }}>
         <Typography variant='h3' aling='center'>
           Отец-прародитель
         </Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-around'}}>
           <Typography variant='h6' aling='center'>
             Именно так его прозвали в том самом роковом документе 1932 года. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed voluptas iste a hic consequatur repellendus aperiam voluptatum minus eligendi aliquid rem, expedita culpa! Architecto rerum saepe, inventore nam dicta aspernatur!
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate assumenda et consectetur vitae minus dolorum mollitia maxime obcaecati natus vero. Dolor non pariatur asperiores, molestias ex cum nostrum sint magnam dolorum iure hic illo ab commodi magni quod ducimus? Laboriosam minima ex quidem maiores, nemo nesciunt non veniam voluptates perspiciatis facere rerum et consequatur a laudantium veritatis expedita asperiores, necessitatibus inventore iusto voluptatem sequi sit ratione, quae fugiat? Incidunt aspernatur odit voluptate dignissimos, rerum porro molestiae quo, dolorem officia quam atque quisquam maiores autem voluptatum repellendus totam unde temporibus quis culpa aliquam. Doloribus inventore quas consequatur nulla ipsa mollitia eos?
           </Typography>
         </Box>
+
       </Box>
     </Box>
   )
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import './style.css'
-import { Box, Typography, IconButton, Drawer } from '@mui/material'
+import { Box, Typography, IconButton, Drawer, Stack, Button } from '@mui/material'
 import DensityMediumIcon from '@mui/icons-material/DensityMedium'
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 const Header = ({ video, title }) => {
     const [scroll, setScroll] = useState(0)
     const [open, setOpen] = useState(false)
+    const [open2, setOpen2] = useState(false)
     const delta = window.innerHeight * 0.3
 
     const handleScroll = () => {
@@ -33,16 +35,43 @@ const Header = ({ video, title }) => {
             >
                 <DensityMediumIcon />
             </IconButton>
+            <Drawer anchor='left' open={open2} onClose={() => setOpen2(false)} >
+                <Box sx={{ px: 20, pt: 2, width: '30vw', height: '100%', bgcolor: 'secondary.main', width: '100%' }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }} >
+                        <Typography color="primary">text</Typography>
+                        <IconButton
+                            edge='end'
+                            onClick={() => setOpen2(false)}
+                            sx={{
+                                zIndex: 3,
+                                position: 'right',
+                                top: 0,
+                                left: 0,
+                                color: 'primary.main'
+                            }}>
+                            <ArrowRightIcon />
+                        </IconButton>
+                    </Box>
+                </Box>
+            </Drawer>
             <Drawer
                 anchor='left'
                 open={open}
                 onClose={() => setOpen(false)}
             >
-                <Box sx={{ px: 20, pt: 2, height: '100%', bgcolor: 'secondary.main'}}>
-                    <Typography variant='h5'>
+
+                <Box sx={{ px: 2, pt: 2, height: '100%', width: '30vw', bgcolor: 'secondary.main' }}>
+
+                    <Typography variant='h5' color="primary">
                         Menu
                     </Typography>
+                    <Stack direction="column" spacing={2} sx={{ width: '100%' }}>
+                        <Button variant="outlined" fullWidth startIcon={<ArrowRightIcon />} onClick={() => setOpen2(true)}>Проекты</Button>
+                        <Button variant="outlined" fullWidth startIcon={<ArrowRightIcon />}>О нас</Button>
+                        <Button variant="outlined" fullWidth startIcon={<ArrowRightIcon />}>грохочет</Button>
+                    </Stack>
                 </Box>
+
             </Drawer>
             <Box
                 sx={{
